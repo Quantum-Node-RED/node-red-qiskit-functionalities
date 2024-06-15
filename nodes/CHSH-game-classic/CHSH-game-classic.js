1de72fd7b1fa158d76df3d36d46071b1f9574877
liaolihao <3144743728@qq.com>
2024-06-15 14:28:43 +0100

    feat:Implementing of the CHSH game classic strategy

--- nodes/CHSH-game-classic/CHSH-game-classic.js
+++ nodes/CHSH-game-classic/CHSH-game-classic.js
@@ -1,36 +1,24 @@
 module.exports = function(RED) {
-    function CHSHClassicNode(config) {
-        RED.nodes.createNode(this,config);
-        var node = this;
-        node.on('input', function(msg) {
-            let x = msg.x
-            let y = msg.y
+  function CHSHGameClassicNode(config) {
+      RED.nodes.createNode(this,config);
+      var node = this;
+      node.on('input', function(msg) {
+        let x = msg.payload.x
+        let y = msg.payload.y
+          
+        let a = 1; // Alice's answer
+        let b = 1;
+       
 
-            let a = 1
-            let b = 1
-            if (x == 0 || y == 0) {
-                msg.result = "win"
-            }else{
-                msg.result = "lose"
-            }
-            msg.a = a
-            msg.b = b
-            node.send(msg);
-        });
-    }
-    RED.nodes.registerType("CHSH-game-classic",CHSHClassicNode);
-
-
-
-
-
-
-
-
-
-
-
-
-
+        if(x == 0 || y == 0){
+          msg.payload.result = "win"
+        }else{
+          msg.payload.result = "lose"
+        }
+        msg.payload.a = a;
+        msg.payload.b = b;
+        node.send(msg);
+      });
+  }
+  RED.nodes.registerType("CHSH-game-classic",CHSHGameClassicNode);
 }
-
