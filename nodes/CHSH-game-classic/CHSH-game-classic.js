98469b074b25d00ad54d41cdd445734d888c5f19
Jiachen Fan <2452665394@qq.com>
2024-06-15 15:30:04 +0100

    Change message attributes

--- nodes/CHSH-game-classic/CHSH-game-classic.js
+++ nodes/CHSH-game-classic/CHSH-game-classic.js
@@ -3,20 +3,24 @@ module.exports = function(RED) {
       RED.nodes.createNode(this,config);
       var node = this;
       node.on('input', function(msg) {
-        let x = msg.payload.x
-        let y = msg.payload.y
+        msg.x = msg.x || 0;
+        msg.y = msg.y || 0;
+
+
+        let x = msg.x
+        let y = msg.y
           
         let a = 1; // Alice's answer
         let b = 1;
        
 
         if(x == 0 || y == 0){
-          msg.payload.result = "win"
+          msg.result = "win"
         }else{
-          msg.payload.result = "lose"
+          msg.result = "lose"
         }
-        msg.payload.a = a;
-        msg.payload.b = b;
+        msg.a = a;
+        msg.b = b;
         node.send(msg);
       });
   }
