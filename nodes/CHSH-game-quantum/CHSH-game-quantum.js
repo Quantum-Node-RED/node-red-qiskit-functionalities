ce05b3cad43fca86324b4c0fa298eb91ea3a98f6
Switt Wattanalekhawong <switt.wattanalekhawong@kcl.ac.uk>
2024-06-18 23:03:00 +0100

    Implement CHSH game using Qiskit

--- nodes/CHSH-game-quantum/CHSH-game-quantum.js
+++ nodes/CHSH-game-quantum/CHSH-game-quantum.js
@@ -1,25 +1,31 @@
-const runPythonScript = require('../pythonShell');
+const runPythonScript = require("../pythonShell");
 
 module.exports = function (RED) {
   function CHSHGameQuantumNode(config) {
     RED.nodes.createNode(this, config);
     var node = this;
-    node.on('input', async function (msg) {
+    node.on("input", async function (msg) {
       const result = await new Promise((resolve, reject) => {
         const option = {
-          "x": msg.payload.x, "y": msg.payload.y
-        }
-        runPythonScript(__dirname, 'test.py', option, (err, results) => {
-          if (err) throw err;
-          return resolve(results);
-        });
+          x: msg.payload.x,
+          y: msg.payload.y,
+        };
+        runPythonScript(
+          __dirname,
+          "CHSH-game-quantum.py",
+          option,
+          (err, results) => {
+            if (err) throw err;
+            return resolve(results);
+          }
+        );
       });
 
       const newMsg = {
-        payload: result
+        payload: result,
       };
       node.send(newMsg);
     });
   }
   RED.nodes.registerType("CHSH-game-quantum", CHSHGameQuantumNode);
-}
+};
