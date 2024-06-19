4f2364ac8154e4ab517813f1506d05fb4cb42a99
wangyiyuelva <yiyuwang891120@gmail.com>
2024-06-19 09:55:43 +0100

    chore: run lint

--- nodes/CHSH-game-classic/CHSH-game-classic.js
+++ nodes/CHSH-game-classic/CHSH-game-classic.js
@@ -1,26 +1,26 @@
-module.exports = function(RED) {
+module.exports = function (RED) {
   function CHSHGameClassicNode(config) {
-      RED.nodes.createNode(this,config);
-      var node = this;
-      node.on('input', function(msg) {
-        msg.payload.x = msg.payload.x || 0;
-        msg.payload.y = msg.payload.y || 0;
+    RED.nodes.createNode(this, config);
+    var node = this;
+    node.on('input', function (msg) {
+      msg.payload.x = msg.payload.x || 0;
+      msg.payload.y = msg.payload.y || 0;
 
 
-        let x = msg.payload.x
-        let y = msg.payload.y
-          
-        let a = 1; // Alice's answer
-        let b = 1;
-       
+      let x = msg.payload.x
+      let y = msg.payload.y
 
-        if(x == 0 || y == 0){
-          msg.payload.result = {a:1,b:1,result:"win"}
-        }else{
-          msg.payload.result = {a:1,b:1,result:"lose"}
-        }
-        node.send(msg);
-      });
+      let a = 1; // Alice's answer
+      let b = 1;
+
+
+      if (x == 0 || y == 0) {
+        msg.payload.result = { a: 1, b: 1, result: "win" }
+      } else {
+        msg.payload.result = { a: 1, b: 1, result: "lose" }
+      }
+      node.send(msg);
+    });
   }
-  RED.nodes.registerType("CHSH-game-classic",CHSHGameClassicNode);
+  RED.nodes.registerType("CHSH-game-classic", CHSHGameClassicNode);
 }
