c612f334b5f318842570361b0cbcf595956b3435
liaolihao <3144743728@qq.com>
2024-06-28 20:16:23 +0800

    feat: the target can receive different number of qubits now

--- nodes/algorithms/AmplificationProblem/amplification_problem.py
+++ nodes/algorithms/AmplificationProblem/amplification_problem.py
@@ -10,36 +10,30 @@ from qiskit_algorithms import Grover
 data = json.loads(sys.argv[1])
 target = data["target"]
 
-oracle = QuantumCircuit(2)
-if target == '11':
-    oracle.cz(0, 1)
-elif target == '10':
-    oracle.x(1)
-    oracle.cz(0, 1)
-    oracle.x(1)
-elif target == '01':
-    oracle.x(0)
-    oracle.cz(0, 1)
-    oracle.x(0)
-elif target == '00':
-    oracle.x([0, 1])
-    oracle.cz(0, 1)
-    oracle.x([0, 1])
-else:
-    raise ValueError("Invalid target state")
+num_qubits = len(target)
+oracle = QuantumCircuit(num_qubits)
+
+for i, char in enumerate(target):
+    if char == '0':
+        oracle.x(i)  
+oracle.mcx(list(range(num_qubits-1)), num_qubits-1)  
+for i, char in enumerate(target):
+    if char == '0':
+        oracle.x(i)  
 
 backend = Aer.get_backend('qasm_simulator')
 
-grover_circuit = QuantumCircuit(2)
-grover_circuit.h([0, 1])
-grover_circuit.append(oracle, [0, 1])   
-grover_circuit.h([0, 1])
+grover_circuit = QuantumCircuit(num_qubits)
+grover_circuit.h(range(num_qubits))
+grover_circuit.append(oracle, range(num_qubits))
+grover_circuit.h(range(num_qubits))
 
 problem = AmplificationProblem(grover_circuit, is_good_state=lambda bitstring: bitstring == target)
 
 grover = Grover(sampler=Sampler())
 result = grover.amplify(problem)
 
+# The prossibility of the each state
 result = {"oracle_evaluation": result.oracle_evaluation, "top_measurement": result.top_measurement,
           "circuit_results":result.circuit_results}
 print(json.dumps(result))
