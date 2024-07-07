8f7a7cab2fdb7913d616a4bde06dd4b0e266ddf0
Jiachen Fan <2452665394@qq.com>
2024-07-07 20:07:54 +0100

    Implement tutorial flow "Add calibration to your circuit".

---
+++ nodes/documentation/circuits-and-operators/other-circuit-building-tools/Pulse_Build_Calibration.js
@@ -0,0 +1,32 @@
+const runPythonScript = require("../../../pythonShell.js");
+
+
+module.exports = function (RED) {
+  function PulseBuildCalibrationNode(config) {
+    RED.nodes.createNode(this, config);
+
+    var node = this;
+
+    node.on('input',  async function (msg) {
+      const result = await new Promise((resolve,reject) => {
+
+        
+        runPythonScript(__dirname, "Pulse_Build_Calibration.py", arg = null, (err, results) => {
+          if (err) throw err;
+          return resolve(results);
+        });
+      
+      });
+
+      const newMsg = {
+        payload: result,
+        encoding: 'base64'
+      }
+
+
+      node.send(newMsg);
+    });
+  }
+  RED.nodes.registerType("pulse-build-calibration", PulseBuildCalibrationNode);
+}
+
