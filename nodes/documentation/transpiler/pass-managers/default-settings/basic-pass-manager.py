9e69c9c8cfd3b6a0659fe2ecef1e93a497684bb3
wangyiyuelva <yiyuwang891120@gmail.com>
2024-07-12 16:43:18 +0800

    feat: rename and add flow

--- nodes/documentation/transpiler/pass-managers/default-settings/basic-pass-manager.py
+++ nodes/documentation/transpiler/pass-managers/default-settings/basic-pass-manager.py
@@ -3,23 +3,8 @@ import base64
 import json
 from qiskit import QuantumCircuit
 from qiskit.circuit.library import GroverOperator, Diagonal
-# from qiskit_ibm_runtime import QiskitRuntimeService
 from qiskit_ibm_runtime.fake_provider import FakeSherbrooke
 from qiskit.transpiler.preset_passmanagers import generate_preset_pass_manager
-
-# load_dotenv()
-
-# runtime_token = os.getenv("IBM_RUNTIME_CRED")
-
-# QiskitRuntimeService.save_account(
-#     channel="ibm_quantum",
-#     token=runtime_token,
-#     set_as_default=True,
-#     # Use `overwrite=True` if you're updating your token.
-#     overwrite=True,
-# )
-
-# service = QiskitRuntimeService() 
  
 # Create circuit to test transpiler on
 oracle = Diagonal([1] * 7 + [-1])
@@ -29,13 +14,6 @@ qc = qc.compose(GroverOperator(oracle))
  
 # Add measurements to the circuit
 qc.measure_all()
- 
-# View the circuit
-qc_buffer = io.BytesIO()
-qc.draw(output='mpl').savefig(qc_buffer, format='png')
-qc_buffer.seek(0)
-qc_str = base64.b64encode(qc_buffer.read()).decode('utf-8')
-qc_buffer.close()
 
 # Specify the system to target
 backend = FakeSherbrooke()
@@ -52,7 +30,6 @@ transpiled_circ_str = base64.b64encode(transpiled_circ_buffer.read()).decode('ut
 transpiled_circ_buffer.close()
 
 result = {
-  "qc_image": qc_str, 
   "transpiled_circ_image": transpiled_circ_str,
 }
 
--- nodes/documentation/transpiler/pass-managers/optimization/synthesis-fidelity.py
+++ nodes/documentation/transpiler/pass-managers/optimization/synthesis-fidelity.py
@@ -1,5 +1,3 @@
-import io
-import base64
 import json
 import numpy as np
 from qiskit import QuantumCircuit
@@ -16,46 +14,13 @@ rand_U = UnitaryGate(UU)
 qc = QuantumCircuit(2)
 qc.append(rand_U, range(2))
 qc.swap(0, 1)
-
-# View the circuit
-qc_buffer = io.BytesIO()
-qc.draw("mpl", style="iqp").savefig(qc_buffer, format='png')
-qc_buffer.seek(0)
-qc_str = base64.b64encode(qc_buffer.read()).decode('utf-8')
-qc_buffer.close()
  
 backend = FakeSherbrooke()
- 
-# pass_manager = generate_preset_pass_manager(
-#     optimization_level=0, backend=backend, seed_transpiler=12345
-# )
-# qc_t0_exact = pass_manager.run(qc)
-# qc_t0_exact.draw("mpl", idle_wires=False)
-
-# pass_manager = generate_preset_pass_manager(
-#     optimization_level=1, backend=backend, seed_transpiler=12345
-# )
-# qc_t1_exact = pass_manager.run(qc)
-# qc_t1_exact.draw("mpl", idle_wires=False)
-
-# pass_manager = generate_preset_pass_manager(
-#     optimization_level=2, backend=backend, seed_transpiler=12345
-# )
-# qc_t2_exact = pass_manager.run(qc)
-# qc_t2_exact.draw("mpl", idle_wires=False)
 
 pass_manager = generate_preset_pass_manager(
     optimization_level=3, backend=backend, seed_transpiler=12345
 )
 qc_t3_exact = pass_manager.run(qc)
-
-# View the circuit
-qc_buffer = io.BytesIO()
-qc_t3_exact.draw("mpl", idle_wires=False).savefig(qc_buffer, format='png')
-qc_buffer.seek(0)
-qc_t3_str = base64.b64encode(qc_buffer.read()).decode('utf-8')
-qc_buffer.close()
-
 
 pass_manager = generate_preset_pass_manager(
     optimization_level=3,
@@ -64,13 +29,6 @@ pass_manager = generate_preset_pass_manager(
     seed_transpiler=12345,
 )
 qc_t3_approx = pass_manager.run(qc)
-
-# View the circuit
-qc_buffer = io.BytesIO()
-qc_t3_approx.draw("mpl", idle_wires=False).savefig(qc_buffer, format='png')
-qc_buffer.seek(0)
-qc_t3_approx_str = base64.b64encode(qc_buffer.read()).decode('utf-8')
-qc_buffer.close()
 
 # Reduce circuits down to 2 qubits so they are easy to simulate
 qc_t3_exact_small = QuantumCircuit.from_instructions(qc_t3_exact)
@@ -85,9 +43,6 @@ approx_fid = trace_to_fid(
 )
 
 result = {
-  "qc_image": qc_str, 
-  "qc_t3_image": qc_t3_str,
-  "qc_t3_approx_image": qc_t3_approx_str,
   "synthesis_fidelity_exact": round(exact_fid, 3),
   "synthesis_fidelity_approx": round(approx_fid, 3)
 }
