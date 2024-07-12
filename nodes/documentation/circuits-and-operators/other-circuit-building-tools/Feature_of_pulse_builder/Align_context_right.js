fc032215b3cdc9545b0d7e3fe78ddb11bd17f9bf
Jiachen Fan <2452665394@qq.com>
2024-07-12 17:30:45 +0100

    Implement feature-of-pulse-builder flow

---
+++ nodes/documentation/circuits-and-operators/other-circuit-building-tools/Feature_of_pulse_builder/Align_context_right.js
@@ -0,0 +1,31 @@
+const runPythonScript = require("../../../../pythonShell.js");
+
+
+module.exports = function (RED) {
+  function AlignRightNode(config) {
+    RED.nodes.createNode(this, config);
+
+    var node = this;
+
+    node.on('input',  async function (msg) {
+      const result = await new Promise((resolve,reject) => {
+
+        
+        runPythonScript(__dirname, "Align_context_right.py", arg = null, (err, results) => {
+          if (err) throw err;
+          return resolve(results);
+        });
+      
+      });
+
+      const newMsg = {
+        payload: result
+      }
+
+
+      node.send(newMsg);
+    });
+  }
+  RED.nodes.registerType("pulse-builder-align-right", AlignRightNode);
+}
+
