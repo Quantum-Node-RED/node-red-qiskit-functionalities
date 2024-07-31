2f5393ee14fb31ae0e4080b2d8078ba0fe7c1d2e
nlepcha23 <norbu.lepcha.23@ucl.ac.uk>
2024-07-31 18:33:00 +0100

    fix: add missing root component to the output

--- interactive-nodes/Quantum-Circuit/Quantum-Circuit-End/Quantum-Circuit-End.js
+++ interactive-nodes/Quantum-Circuit/Quantum-Circuit-End/Quantum-Circuit-End.js
@@ -36,7 +36,11 @@ module.exports = function (RED) {
       // Check if all expected qubits have been received
       if (state.receivedQubits === state.expectedQubits) {
         const output = [];
-
+        //Add root compoenent to the structure output
+        output.push(
+          currentStructure.find((node) => node.name === "root")
+        );
+        // Add Quantum_Circuit_Begin component to the structure output
         output.push(
           currentStructure.find((node) => node.name === "Quantum_Circuit_Begin")
         );
