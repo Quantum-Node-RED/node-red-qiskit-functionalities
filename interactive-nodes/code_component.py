e7a3c1fc185a5dfd27511dbfadf452644a1cea5a
wangyiyuelva <yiyuwang891120@gmail.com>
2024-08-01 03:16:53 +0800

    fix: add more operations into code generator

--- interactive-nodes/code_component.py
+++ interactive-nodes/code_component.py
@@ -4,19 +4,19 @@
 #Tools_components will be their own class
 #Circuit_components will be their own class
 class Code_Component:
-    #Circuits
+    # Circuits
     Quantum_Circuit_Begin = "{circuit_name} = QuantumCircuit({num_qubits})"
     measure = "{circuit_name}.meausre({qbit})"
     swap = "{circuit_name}.swap({qbit1}, {qbit2})"
     classical_register= "{var_name} = ClassicalRegister({num_qubits})"
     quantum_register= "{var_name} = QuantumRegister({num_qubits})"
     reset= "{circuit_name}.reset({qbit})"
-    #Maths
+    # Maths
     matrix = "{var_name} = np.array({matrix})"
-    #Gates
+    # Gates
     CX_gate = "{circuit_name}.cx({qbit1}, {qbit2})"
     CZ_gate = "{circuit_name}.cz({qbit1}, {qbit2})"
-    CU_gate= "{circuit_name}.cu({theta}, {phi}, {lambda}, {qbit1}, {qbit2})"
+    CU_gate= "{circuit_name}.cu({theta}, {phi}, {lam}, {qbit1}, {qbit2})"
     H_gate = "{circuit_name}.h({qbit})"
     RX_gate = "{circuit_name}.rx({theta}, {qbit})"
     RZ_gate = "{circuit_name}.rz({theta}, {qbit})"
@@ -26,17 +26,17 @@ class Code_Component:
     barrier = "{circuit_name}.barrier({qbit})"
     phase = "{circuit_name}.p({theta}, {qbit})"
     I_gate = "{circuit_name}.i({qbit})"
-    U_gate = "{circuit_name}.u({theta}, {phi}, {lambda}, {qbit})"
+    U_gate = "{circuit_name}.u({theta}, {phi}, {lam}, {qbit})"
     Toffoli_gate = "{circuit_name}.toffoli({qbit1}, {qbit2}, {qbit3})"
     CCX_gate = "{circuit_name}.ccx({qbit1}, {qbit2}, {qbit3})"
     mutli_controlled_U_gate="from qiskit.circuit.library import UGate\n{circuit_name}.mct({qbit1}, {qbit2}, {qbit3})"
-    #Tools
+    # Tools
     local_simulator = """
                 default='qasm_simulator'
                 {var_name} = Aer.get_backend({simulator} or default)
                 {var_name_result} = execute({circuit_name}, backend = {var_name}, shots = %s).result()
-                {var_name_counts}= {var_name_result}.get_counts()
-                print(var_name_counts)
+                {var_name_counts} = {var_name_result}.get_counts()
+                print({var_name_counts})
                 """
     draw= "{circuit_name}.draw(output='{output_type}')"
     encode_image = """
@@ -61,5 +61,5 @@ class Code_Component:
 
 
     def code_import(self):
-        import_code = "import numpy as np\nfrom qiskit import QuantumCircuit"
+        import_code = "import numpy as np\nfrom qiskit import QuantumCircuit\n"
         return import_code
