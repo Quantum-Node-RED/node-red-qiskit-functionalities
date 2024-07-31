a7eb5b9991b176c1af0695f669eaf677f921fa91
nlepcha23 <norbu.lepcha.23@ucl.ac.uk>
2024-07-31 18:07:55 +0100

    minor: add parameter expectedQubits to Quantum_Circuit_Begin

--- interactive-nodes/Quantum-Circuit/Quantum-Circuit-Begin/Quantum-Circuit-Begin.js
+++ interactive-nodes/Quantum-Circuit/Quantum-Circuit-Begin/Quantum-Circuit-Begin.js
@@ -10,12 +10,15 @@ module.exports = function (RED) {
 
       // Store the number of expected qubits in flow context
       node.context().flow.set("expectedQubits", 0);
-
+      // Count the number of wires connected to this node
+      let connectedPaths=node._wireCount || 0;
       // Add the Quantum Circuit Begin component
       const Quantum_Circuit_Begin_component = new component.Component(
         "Quantum_Circuit_Begin",
         {}
       );
+      // Set parameter the expected qubits for the Quantum Circuit Begin component
+      Quantum_Circuit_Begin_component.parameters["expectedQubits"]=connectedPaths;
       component.addComponent(msg, Quantum_Circuit_Begin_component);
 
       // Send the message with the updated payload
