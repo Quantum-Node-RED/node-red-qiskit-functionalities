83a0f77c173d9a16012761805f6de723735513c2
liaolihao <3144743728@qq.com>
2024-08-03 17:24:48 +0800

    feat:add quantum-register component and its parameters

---
+++ interactive-nodes/Functions/Quantum-register/Quantum-register.js
@@ -0,0 +1,22 @@
+const component=require('../../component.js');
+const constants = require('../../constants.js');
+
+module.exports = function (RED) {
+    function Quantum_registerNode(config) {
+      RED.nodes.createNode(this, config);
+      var node = this;
+
+      const var_name = config.name;
+      const num_qubits = config.num_qubits;
+
+      node.on('input', function (msg) {
+        msg.payload = msg.payload || {};
+        const quantum_register_component = new component.Component(constants.QUANTUM_REGISTER_COMPONENT_NAME,{});
+        quantum_register_component.parameters["var_name"] = var_name;
+        quantum_register_component.parameters["num_bits"] = num_qubits;
+        component.addComponent(msg, quantum_register_component);
+        node.send(msg);
+      });
+    }
+    RED.nodes.registerType("Quantum-register", Quantum_registerNode);
+  }
