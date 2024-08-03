cd5b9815c95fb167260434c007e8f308ebfe1e2a
liaolihao <3144743728@qq.com>
2024-08-03 17:34:29 +0800

    feat: add reset component and its parameters

---
+++ interactive-nodes/Quantum-Circuit/reset/reset.js
@@ -0,0 +1,25 @@
+const component = require("../../component.js");
+const constants = require('../../constants.js');
+
+module.exports = function (RED) {
+  function resetNode(config) {
+    RED.nodes.createNode(this, config);
+    var node = this;
+    const qbits = config.reset_qubits
+
+    node.on("input", function (msg) {
+      msg.payload = msg.payload || {};
+
+      const reset_component = new component.Component(constants.RESET_COMPONENT_NAME, {});
+      // qubit: qubit(s) to reset
+      reset_component.parameters["qbit"] = qbits;
+
+      component.addComponent(msg, reset_component);
+
+      // Send the message onward
+      node.send(msg);
+    });
+  }
+
+  RED.nodes.registerType("reset", resetNode);
+};
---
+++ interactive-nodes/test.py
@@ -0,0 +1,4 @@
+from qiskit import QuantumCircuit
+
+c = QuantumCircuit(2)
+c.reset
