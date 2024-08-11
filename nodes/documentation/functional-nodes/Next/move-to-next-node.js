705451edbfb2ac64c5eb913ebf9f593c2782cb44
Jiachen Fan <2452665394@qq.com>
2024-08-11 13:33:43 +0100

    Fix bug of 'next' node

--- nodes/documentation/functional-nodes/Next/move-to-next-node.js
+++ nodes/documentation/functional-nodes/Next/move-to-next-node.js
@@ -1,38 +1,48 @@
 // const runPythonScript = require("../.././../../pythonShell");
-var recievedMsg = null;
+// var recievedMsg = null;
 
 module.exports = function (RED) {
   function MoveToNextNode(config) {
     RED.nodes.createNode(this, config);
     var node = this;
     node.name = config.name;
+
+    node.context().set("recievedMsg", null);
     
-    node.on("input", async function (msg) {
-      recievedMsg = msg;
+    node.on("input",  function (msg) {
+      try {
+        node.context().set("recievedMsg", msg);
+        // console.log('Message received and processed');
+      } catch (err) {
+        node.error('Error processing input message:', err);
+      }
     });
 
     node.on("close", function () {
-      recievedMsg = null;
+      node.removeAllListeners("input");
+      node.context().set("recievedMsg", null);
     });
   }
   RED.nodes.registerType("move-to-next-node", MoveToNextNode);
   
   RED.httpAdmin.post('/Next/:id', RED.auth.needsPermission('inject.write'), function (req, res) {
-    const node = RED.nodes.getNode(req.params.id)
+    const node = RED.nodes.getNode(req.params.id);
     if (node) {
       try {
+        const recievedMsg = node.context().get("recievedMsg");
         if (recievedMsg){
           var msg = { payload: "Next" }
-          node.send(msg)
+          node.send(msg);
+          node.done();
         }else{
-          node.error(RED._('Please start from the beginning'))
+          node.error(RED._('Please start from the beginning'));
         }
       } catch (err) {
-        res.sendStatus(500)
-        node.error(RED._('nameinject.failed', { error: err.toString() }))
+        res.sendStatus(500);
+        // node.error(RED._('nameinject.failed', { error: err.toString() }));
       }
     } else {
-      res.sendStatus(404)
+      res.sendStatus(404);
     }
   });
 };
