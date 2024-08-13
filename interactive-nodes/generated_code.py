63652638a3c66d175aa1057e6767fb319edb9a00
Switt Wattanalekhawong <switt.wattanalekhawong@kcl.ac.uk>
2024-08-13 16:51:29 +0100

    Implement QAOA at gate level

--- interactive-nodes/generated_code.py
+++ interactive-nodes/generated_code.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import io
 import json
 from qiskit import QuantumCircuit
@@ -6,23 +5,6 @@ import base64
 import matplotlib.pyplot as plt
 import base64
 import io
-
-
-
-# Quantum Circuit Begin test
-test = QuantumCircuit(2)
-test.cx(1, 0)
-test.h(0)
-test.cz(0, 1)
-test.rx(0.5, 1)
-# Quantum Circuit End
-buffer = io.BytesIO()
-test.draw(output='mpl').savefig(buffer, format='png')
-buffer.seek(0)
-b64_str = base64.b64encode(buffer.read()).decode('utf-8')
-buffer.close()
-print(json.dumps(b64_str))
-=======
 from qiskit.primitives import Sampler
 from qiskit.quantum_info import Pauli
 from qiskit.result import QuasiDistribution
