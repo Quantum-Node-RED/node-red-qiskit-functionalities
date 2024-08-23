e3f3f2fad305fb63ed752e26a98a5be247a758cc
Jiachen Fan <2452665394@qq.com>
2024-08-23 16:19:56 +0100

    Clean up code for "Get started with Qiskit transpiler" and "Advanced Transpiler Resources" section

---
+++ Learning/nodes/Transpiler/Get-Started-With-Qiskit-Transpiler/Transpile-With-Pass-Managers/Pass-Manager/Pass_manager.js
@@ -0,0 +1,37 @@
+const runPythonScript = require("../../../../pythonShell");
+
+module.exports = function (RED) {
+  function PassmanageNode(config) {
+    RED.nodes.createNode(this, config);
+    this.token = config.token;
+
+    var node = this;
+    node.status({ fill: "red", shape: "dot", text: "You haven't learned this node yet." });
+
+    node.on('input', async function (msg) {
+      node.status({ fill: "green", shape: "dot", text: "You have  learned this node." });
+      const result = await new Promise((resolve, reject) => {
+        const options = {
+          token: node.token
+        };
+        runPythonScript(__dirname, "Pass_manager.py", options, (err, results) => {
+          if (err) {
+            node.error("Error running Python script: " + err);
+            return reject(err);
+          }
+          return resolve(results);
+        });
+      });
+
+      const newMsg = {
+        payload: result
+      };
+      node.send(newMsg);
+    });
+
+    node.on('close', function () {
+      node.status({ fill: "red", shape: "dot", text: "You haven't learned this node yet." });
+    });
+  }
+  RED.nodes.registerType("pass_manage", PassmanageNode);
+}
