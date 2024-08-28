52a60d44f357ffa440d840bb584a3fd0d8625c10
telmagudmundsdottir99 <telmagudmundsdottir99@gmail.com>
2024-08-28 18:30:12 +0100

    feat:moved transpiler\pass-manager to learning folder

---
+++ Learning/nodes/Transpiler/Configure-Preset-Pass-Managers/Set-Optimization-Level/Random-Unitary/Random_unitary.js
@@ -0,0 +1,32 @@
+const runPythonScript = require("../../../../pythonShell");
+
+module.exports = function (RED) {
+  function transpilationRandomUnitary(config) {
+    RED.nodes.createNode(this, config);
+    var node = this;
+
+    node.status({ fill: "red", shape: "dot", text: "You haven't learned this node yet." });
+
+    node.on("input", async function (msg) {
+      node.status({ fill: "green", shape: "dot", text: "You have learned this node." });
+      const result = await new Promise((resolve, reject) => {
+        const option = {
+        };
+        runPythonScript(__dirname, "Random_unitary.py", option, (err, results) => {
+          if (err) throw err;
+          return resolve(results);
+        });
+      });
+
+      const newMsg = {
+        payload: result
+      };
+      node.send(newMsg);
+    });
+
+    node.on('close', function () {
+      node.status({ fill: "red", shape: "dot", text: "You haven't learned this node yet." });
+    });
+  }
+  RED.nodes.registerType("transpilation-random-unitary", transpilationRandomUnitary);
+};
